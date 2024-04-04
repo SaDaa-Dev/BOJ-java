7a471f9082e2068f767d79631d5955c078543968
ChanBook <qudcks3000@gmail.com>
2024-04-04 16:16:31 +0900

    first commit

---
+++ src/BOJ3003.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ3003 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] origin = new int[] {1, 1, 2, 2, 2, 8};
+
+        String str = br.readLine();
+        int[] target = Arrays.stream(str.split(" ")).mapToInt(Integer::valueOf).toArray();
+        for (int i = 0; i < origin.length; i++) {
+            origin[i] = origin[i] - target[i] != 0 ? origin[i] - target[i] : 0;
+        }
+
+        Arrays.stream(origin).forEach(value -> System.out.print(value + " "));
+    }
+}
