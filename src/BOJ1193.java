ca85460522eea9dfbb7a55567ad0adc6d0c86d3c
ChanBook <qudcks3000@gmail.com>
2024-07-26 16:01:41 +0900

    BOJ1193-fix

--- src/BOJ1193.java
+++ src/BOJ1193.java
@@ -7,22 +7,24 @@ public class BOJ1193 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        int current = 1;  // 현재 몇 번째 분수인지
-        int level = 1;  // 대각선 레벨
+        int cross_cnt = 1, prev_cnt = 0;
 
-        while (current + level < n) {
-            current += level;
-            level++;
+        while (true) {
+
+            if (n <= cross_cnt + prev_cnt) {
+                if (cross_cnt % 2 == 1) {
+                    System.out.println((cross_cnt - (n - prev_cnt - 1)) + "/" + (n - prev_cnt));
+                    break;
+                } else {
+                    System.out.print((n - prev_cnt) + "/" + (cross_cnt - (n - prev_cnt - 1)));
+                    break;
+                }
+            } else {
+                prev_cnt += cross_cnt;
+                cross_cnt++;
+            }
         }
 
-        int index = n - current;  // 현재 레벨에서 몇 번째인지
 
-        // level이 홀수라면 분모가 더 크고 위에서 아래로 내려갑니다.
-        // level이 짝수라면 분자가 더 크고 아래에서 위로 올라갑니다.
-        if (level % 2 == 1) {
-            System.out.println((level - index) + "/" + (index + 1));
-        } else {
-            System.out.println((index + 1) + "/" + (level - index));
-        }
     }
 }
