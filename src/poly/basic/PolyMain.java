044805254258c5f400dfe62e5b61f2684b3dcb32
ChanBook <qudcks3000@gmail.com>
2024-08-02 11:43:46 +0900

    다형성 참조

---
+++ src/poly/basic/PolyMain.java
@@ -0,0 +1,17 @@
+package poly.basic;
+
+public class PolyMain {
+    public static void main(String[] args) {
+        Parent parent = new Parent();
+        parent.parentMethod();
+
+        Child child = new Child();
+        child.parentMethod();
+        child.childMethod();
+
+        Parent poly = new Child();
+        poly.parentMethod();
+
+        Child childPoly = (Child) poly;
+    }
+}
