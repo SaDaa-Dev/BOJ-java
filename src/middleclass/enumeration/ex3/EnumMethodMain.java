de18cb3f1bce793d08cdf74d9cc0729d86a20b21
ChanBook <qudcks3000@gmail.com>
2024-08-08 14:10:17 +0900

    열거형 리팩토링

---
+++ src/middleclass/enumeration/ex3/EnumMethodMain.java
@@ -0,0 +1,15 @@
+package middleclass.enumeration.ex3;
+
+import java.util.Arrays;
+
+public class EnumMethodMain {
+    public static void main(String[] args) {
+        Grade[] values = Grade.values();
+        System.out.println("values = " + Arrays.toString(values));
+
+        // String -> Enum 변환
+        String input = "GOLD";
+        Grade gold = Grade.valueOf(input);
+        System.out.println("gold = " + gold);
+    }
+}
---
+++ src/middleclass/enumeration/ex3/Grade.java
@@ -0,0 +1,5 @@
+package middleclass.enumeration.ex3;
+
+public enum Grade {
+    BASIC, GOLD, DIAMOND
+}
