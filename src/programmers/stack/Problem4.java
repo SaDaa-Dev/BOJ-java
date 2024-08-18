3136f22fc1383baf85a44e8aeca2d4022ca73778
ChanBook <qudcks3000@gmail.com>
2024-08-18 16:51:32 +0900

    프로그래머스 스택/큐 문제4

--- src/programmers/stack/Problem4.java
+++ src/programmers/stack/Problem4.java
@@ -1,25 +1,44 @@
 package programmers.stack;
 
-import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.Queue;
 
 public class Problem4 {
     public static void main(String[] args) {
-
+        int solution = solution(new int[]{2, 1, 3, 2}, 2);
+        System.out.println("solution = " + solution);
     }
 
     private static int solution(int[] priorities, int location) {
-        LinkedList<Integer> processes = new LinkedList<>();
-        Iterator<Integer> iter = processes.iterator();
+        Queue<int[]> queue = new LinkedList<>();
 
-        while (processes.isEmpty()) {
-            Integer first = processes.remove(0);
-            if (processes.stream().anyMatch(a -> a.intValue() > first)) {
+        for (int i = 0; i < priorities.length; i++) {
+            queue.add(new int[] {priorities[i], i});
+        }
+        int answer = 0;
 
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+
+            boolean hasHigherPrior = false;
+            for (int[] process : queue) {
+                if (process[0] > current[0]) {
+                    hasHigherPrior = true;
+                    break;
+                }
             }
 
+            if (hasHigherPrior) {
+                queue.add(current);
+            } else {
+                answer++;
+
+                if (current[1] == location) {
+                    return answer;
+                }
+            }
         }
 
-        return 0;
+        return answer;
     }
 }
