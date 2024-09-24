3d24e0e7cc592ebb14bfaab50e432da69d52dfd0
ChanBook <qudcks3000@gmail.com>
2024-09-24 15:19:12 +0900

    프로그래머스 스택/큐 다리를 지나는 트럭

--- src/programmers/stack/Problem5.java
+++ src/programmers/stack/Problem5.java
@@ -1,2 +1,40 @@
-package programmers.stack;public class Problem5 {
+package programmers.stack;
+
+import javax.naming.LinkException;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Problem5 {
+    public static void main(String[] args) {
+        int[] data = {7,4,5,6};
+        System.out.println("solution({7,4,5,6}) = " + solution(2, 10, data));
+    }
+
+    private static int solution(int bridge_length, int weight, int[] truck_weights) {
+       Queue<Integer> bridge = new LinkedList<>();
+       int time = 0;
+       int weightOnBridge = 0;
+       int idx = 0;
+
+        for (int i = 0; i < bridge_length; i++) {
+            bridge.add(0);
+        }
+
+        while (!bridge.isEmpty()) {
+            time++;
+            weightOnBridge -= bridge.poll();
+
+            if (idx < truck_weights.length) {
+                if (weightOnBridge + truck_weights[idx] <= weight) {
+                    bridge.add(truck_weights[idx]);
+                    weightOnBridge += truck_weights[idx];
+                    idx++;
+                } else {
+                    bridge.add(0);
+                }
+            }
+        }
+
+        return time;
+    }
 }
