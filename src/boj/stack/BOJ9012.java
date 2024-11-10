e9ff6d9810b1438dacef2fe013bde22e0074e7af
SaDa-Dev <qudcks3000@gmail.com>
2024-11-10 12:15:27 +0900

    BOJ9012

---
+++ src/boj/stack/BOJ9012.java
@@ -0,0 +1,35 @@
+package boj.stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class BOJ9012 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            String input = br.readLine();
+            System.out.println(isValidParentheses(input) ? "YES" : "NO");
+        }
+    }
+
+    private static boolean isValidParentheses(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') {
+                stack.push(ch);
+            } else if (ch == ')') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
