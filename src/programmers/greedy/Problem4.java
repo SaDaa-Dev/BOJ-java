6bb84265fb5184e088d66f85f10f1053d3ef42ed
SaDa-Dev <qudcks3000@gmail.com>
2024-11-18 13:24:59 +0900

    프로그래머스/그리디/문제4/섬 연결하기/MST 알고리즘 프루스칼

---
+++ src/programmers/greedy/Problem4.java
@@ -0,0 +1,80 @@
+package programmers.greedy;
+
+import java.util.Arrays;
+
+public class Problem4 {
+    private class Solution {
+
+        private int[] parent;
+        private int[] rank;
+
+        private int find(int x) {
+            if (parent[x] != x) {
+                parent[x] = find(parent[x]);
+            }
+            return parent[x];
+        }
+
+        private void union(int a, int b) {
+            int rootA = find(a);
+            int rootB = find(b);
+            if (rootA != rootB) {
+                if (rank[rootA] < rank[rootB]) {
+                    parent[rootA] = rootB;
+                } else if (rank[rootA] > rank[rootB]) {
+                    parent[rootB] = rootA;
+                } else {
+                    parent[rootB] = rootA;
+                    rank[rootA]++;
+                }
+            }
+        }
+        public int solution(int n, int[][] costs) {
+            Arrays.sort(costs, (o1, o2) -> o1[2] - o2[2]);
+
+            parent = new int[n + 1];
+            rank = new int[n + 1];
+
+            for (int i = 1; i <= n; i++) {
+                parent[i] = i;
+                rank[i] = 0;
+            }
+
+            int totalCost = 0;
+            int count = 0; // 간선
+
+            for (int[] cost : costs) {
+                int a = cost[0];
+                int b = cost[1];
+                int c = cost[2];
+
+                if (find(a) != find(b)) {
+                    union(a, b);
+                    totalCost += c;
+                    count++;
+
+                    if (count == n - 1) {
+                        break;
+                    }
+                }
+            }
+
+            return totalCost;
+        }
+    }
+    public static void main(String[] args) {
+        Problem4 prom = new Problem4();
+        Solution sol = prom.new Solution();
+        int n1 = 4;
+        int[][] costs = {
+                {1, 2, 1},
+                {2, 3, 4},
+                {1, 3, 3},
+                {1, 4, 2},
+                {4, 3, 5}
+        };
+
+        System.out.println(sol.solution(n1, costs)); // 출력: 7
+    }
+
+}
