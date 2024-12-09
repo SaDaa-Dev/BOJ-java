4be37394251219823d6d651dbce8b11743cd6983
SaDa-Dev <qudcks3000@gmail.com>
2024-12-09 15:19:11 +0900

    프로그래머스/DP/문제1/N으로 표현

--- src/programmers/dynamic/Problem1.java
+++ src/programmers/dynamic/Problem1.java
@@ -1,4 +1,48 @@
 package programmers.dynamic;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Problem1 {
+    private class Solution {
+        public int solution(int N, int number) {
+            Set<Integer>[] dp = new HashSet[9];
+
+            for (int i = 1; i <= 8; i++) {
+                dp[i] = new HashSet<>();
+            }
+
+            int num = 0;
+            for (int i = 1; i <= 8; i++) {
+                num = num * 10 + N;
+                dp[i].add(num);
+            }
+
+            for (int i = 1; i <= 8; i++) {
+                for (int j = 1; j < i; j++) {
+                    for (int a : dp[j]) {
+                        for (int b : dp[i - j]) {
+                            dp[i].add(a + b);
+                            dp[i].add(a - b);
+                            dp[i].add(a * b);
+                            if (b != 0) {
+                                dp[i].add(a / b);
+                            }
+                        }
+                    }
+                }
+
+                if (dp[i].contains(number)) {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+    public static void main(String[] args) {
+        Problem1 prom = new Problem1();
+        Solution sol = prom.new Solution();
+        System.out.println(sol.solution(5, 12));
+    }
 }
---
+++ src/programmers/graph/Problem3.java
@@ -0,0 +1,24 @@
+package programmers.graph;
+
+public class Problem3 {
+    private class Solution {
+        private final int[][] DIRECTION = { // 시계방향
+                {0, 1},
+                {1, 1},
+                {1, 0},
+                {1, -1},
+                {0, -1},
+                {-1, -1},
+                {-1, 0},
+                {-1, 1}
+        };
+        public int solution(int[] arrows) {
+            int answer = 0;
+            return answer;
+        }
+    }
+    public static void main(String[] args) {
+
+
+    }
+}
