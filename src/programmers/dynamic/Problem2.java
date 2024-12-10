810e01938b81f5487899e1c77c922cc0b542bad2
SaDa-Dev <qudcks3000@gmail.com>
2024-12-10 11:29:46 +0900

    프로그래머스/DP/문제2/정수 삼각형

--- src/programmers/dynamic/Problem2.java
+++ src/programmers/dynamic/Problem2.java
@@ -2,33 +2,42 @@ package programmers.dynamic;
 
 public class Problem2 {
     private class Sol {
-        public int solution(String name) {
-            int answer = 0;
-            int length = name.length();
-            int minMove = length - 1;
+        public int solution(int[][] triangle) {
+            int n = triangle.length;
+            int[][] dp = new int[n][n];
+            dp[0][0] = triangle[0][0];
 
-            for (int i = 0; i < length; i++) {
-                char current = name.charAt(i);
-                int changeCnt = Math.min(current - 'A', 'Z' - current + 1);
-                answer += changeCnt;
-
-                int next = i + 1;
-                while (next < length && name.charAt(next) == 'A') {
-                    next++;
+            for (int i = 1; i < n; i++) {
+                for (int j = 0; j <= i; j++) {
+                    if (j == 0) {
+                        dp[i][j] = dp[i - 1][j] + triangle[i][j];
+                    } else if (j == i) {
+                        dp[i][j] = dp[i - 1][j - 1] + triangle[i][j];
+                    } else {
+                        dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j];
+                    }
                 }
-
-                minMove = Math.min(minMove, i * 2 + (length - next));
-                minMove = Math.min(minMove, i + 2 * (length - next));
             }
 
-            answer += minMove;
+            int answer = Integer.MIN_VALUE;
+            for (int j = 0; j < n; j++) {
+                answer = Math.max(answer, dp[n - 1][j]);
+            }
+
             return answer;
         }
     }
     public static void main(String[] args) {
         Problem2 prom = new Problem2();
         Sol sol = prom.new Sol();
+        int[][] input = new int[][]{
+                {7},
+                {3, 8},
+                {8, 1, 0},
+                {2, 7, 4, 4},
+                {4, 5, 2, 6, 5}
+        };
 
-        System.out.println(sol.solution("JEROEN"));
+        System.out.println(sol.solution(input));
     }
 }
