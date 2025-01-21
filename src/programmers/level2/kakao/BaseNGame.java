ca3a2ecdce5088b37c9ebc0360f1b75e91c37fdf
SaDa-Dev <qudcks3000@gmail.com>
2025-01-21 14:24:45 +0900

    프로그래머스/레벨2/N진수게임

---
+++ src/programmers/level2/kakao/BaseNGame.java
@@ -0,0 +1,22 @@
+package programmers.level2.kakao;
+
+public class BaseNGame {
+    public String solution(int n, int t, int m, int p) {
+        StringBuilder answer = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; sb.length() < t * m; i++) {
+            sb.append(Integer.toString(i, n));
+        }
+
+        for (int i = p - 1; answer.length() < t; i = i + m) {
+            answer.append(sb.charAt(i));
+        }
+
+        return answer.toString().toUpperCase();
+    }
+
+    public static void main(String[] args) {
+        BaseNGame problem = new BaseNGame();
+        System.out.println(problem.solution(2, 4, 2, 1));
+    }
+}
